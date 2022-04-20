85c9813448ce3310f7d093a5605ecc9cab389ff2
kaymoore <kaymoore@macbook>
2022-04-20 08:01:12 -0400

    Finish reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let i = str.length-1;
+    let result = "";
 
+    while (i >= 0){
+        result += str.charAt(i);
+        i--;
+    }
+    return result;
 };
 
 // Do not edit below this line
