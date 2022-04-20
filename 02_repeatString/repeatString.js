257d03f983e3b71e7c59988f3210deff3502b3e2
kaymoore <kaymoore@macbook>
2022-04-20 07:53:28 -0400

    Finish repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, numRepeat) {
+    if (numRepeat < 0){
+        return "ERROR";
+    } else if (numRepeat == 0 || str == ""){
+        return "";
+    }
+
+    let result = "";
+    for (let i = 0; i < numRepeat; i++){
+        result += str;
+    }
+    return result;
 
 };
 
