795d8dc787d627919dd4abaaaa942aecee9b1e07
kaymoore <kaymoore@macbook>
2022-04-20 08:11:12 -0400

    Finish removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    for (const num of args){
+        let index = arr.indexOf(num);
+        if (index != -1){
+            arr.splice(index, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
