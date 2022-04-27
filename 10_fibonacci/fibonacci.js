32e63362c9757bc7f35c69306911bc2a536dd5e7
kaymoore <kaymoore@macbook>
2022-04-27 08:51:25 -0400

    Finished 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,34 @@
-const fibonacci = function() {
+/**
+ * 4
+ *  1  2  3  4
+ * [1, 1, 2, 3] 
+ * if (n < 3) -- return fib[n];
+ * while (fib.length < n){
+ *  fib[fib.length] = fib[fib.length-1] + fib[fib.length-2];
+ * }
+ * return fib[fib.length-1]
+ * 
+ * 
+ * 
+ * 
+ * @param {*} n 
+ * @returns 
+ */
+
+
+
+
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if (n < 0){
+        return "OOPS";
+    } 
+
+    let fib = [1,1];
+    while (fib.length < n){
+        fib[fib.length] = fib[fib.length-1] + fib[fib.length-2];
+    }
+    return fib[n-1];
 
 };
 
