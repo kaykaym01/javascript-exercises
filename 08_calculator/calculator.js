ac73009dc91c9508317b7d905de369eddc611a19
kaymoore <kaymoore@macbook>
2022-04-26 22:04:01 -0400

    Finished 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((sum, item) => sum+item, 0);
+}
+
+const multiply = function(arr) {
+  return arr.reduce((product, item) => product*item, 1);
+
 };
 
-const multiply = function() {
-
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n <= 1){
+    return 1;
+  } else {
+    return n * factorial(n-1);
+  }
 };
 
 // Do not edit below this line
