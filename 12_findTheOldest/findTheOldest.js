a20660e8df07a904d8d334060a842161e6bdfd2a
kaymoore <kaymoore@macbook>
2022-04-27 09:13:50 -0400

    Finish 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
+// get the ages using map
+// use reduce 
 
+const findTheOldest = function(people) {
+    return people.reduce((maxPerson, person) =>{
+        return (getAge(person) > getAge(maxPerson)) ? person : maxPerson;
+    }, people[0]);
 };
+
+const getAge = function(person){
+        let startYear = person.yearOfBirth;
+        let endYear = person.hasOwnProperty("yearOfDeath") ? person.yearOfDeath : (new Date()).getFullYear();
+        return endYear - startYear;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
